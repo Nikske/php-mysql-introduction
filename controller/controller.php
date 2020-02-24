e781589e055db0257503befc05e70fdfdb0b16a9
Nikske <wolfvdd@gmail.com>
2020-02-24 16:57:59 +0100

    creating inputs

--- controller/controller.php
+++ controller/controller.php
@@ -7,7 +7,18 @@ error_reporting(E_ALL);
 
 class controller {
     public function render(array $GET, array $POST) {
+        if (isset($_POST['inputFirstName']) && isset($_POST['inputLastName']) && isset($_POST['inputUserName']) && isset($_POST['inputLinkedin']) && isset($_POST['inputGithub']) && isset($_POST['inputEmail']) && isset($_POST['inputLanguage']) && isset($_POST['inputAvatar']) && isset($_POST['inputVideo']) && isset($_POST['inputQuote']) && isset($_POST['inputQuoteAuthor'])) {
+            $this->insert();
+        }
 
         require 'view/homepage.php';
+    }
+
+    private function insert() {
+        $firstName = trim(htmlspecialchars($_POST['inputFirstName']));
+        $lastName = trim(htmlspecialchars($_POST['inputLastName']));
+        $userName = trim(htmlspecialchars($_POST['inputUserName']));
+        $linkedIn = trim(htmlspecialchars($_POST['inputLinkedin']));
+        $github = trim(htmlspecialchars($_POST['inputGithub']));
     }
 }
--- view/homepage.php
+++ view/homepage.php
@@ -11,7 +11,39 @@
             <form method="post">
                 <div class="form-group">
                     <label for="inputFirstName">First name</label>
-                    <input type="text" class="form-control">
+                    <input type="text" id="inputFirstName" class="form-control">
+                    <label for="inputLastName">Last name</label>
+                    <input type="text" id="inputLastName" class="form-control">
+                    <label for="inputUserName">User name</label>
+                    <input type="text" id="inputUserName" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="inputLinkedin">Linkedin</label>
+                    <input type="text" id="inputLinkedin" class="form-control">
+                    <label for="inputGithub">Github</label>
+                    <input type="text" id="inputGithub" class="form-control">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" id="inputEmail" class="form-control">
+                    <label for="inputLanguage">Preferred language</label>
+                    <select class="form-control" id="inputLanguage">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="inputAvatar">Avatar</label>
+                    <input type="text" id="inputAvatar" class="form-control">
+                    <label for="inputVideo">Video</label>
+                    <input type="text" id="inputVideo" class="form-control">
+                    <label for="inputQuote">Quote</label>
+                    <input type="text" id="inputQuote" class="form-control">
+                    <label for="inputQuoteAuthor">Quote author</label>
+                    <input type="text" id="inputQuoteAuthor" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-primary">GO</button>
             </form>
