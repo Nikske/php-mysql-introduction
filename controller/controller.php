5c6a44912cfaddd35f00ba514a3b3391019a46e5
Nikske <wolfvdd@gmail.com>
2020-02-25 14:36:52 +0100

    bound parameters

--- controller/controller.php
+++ controller/controller.php
@@ -7,12 +7,13 @@ error_reporting(E_ALL);
 
 class controller {
     public function render(array $GET, array $POST) {
-        if (isset($_POST['inputFirstName']) && isset($_POST['inputLastName']) && isset($_POST['inputUserName']) && isset($_POST['inputLinkedin']) && isset($_POST['inputGithub']) && isset($_POST['inputEmail']) && isset($_POST['inputLanguage']) && isset($_POST['inputAvatar']) && isset($_POST['inputVideo']) && isset($_POST['inputQuote']) && isset($_POST['inputQuoteAuthor'])) {
-            $this->insert();
-        } else {
-            echo "all fields are required.";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['inputFirstName']) && isset($_POST['inputLastName']) && isset($_POST['inputUserName']) && isset($_POST['inputLinkedin']) && isset($_POST['inputGithub']) && isset($_POST['inputEmail']) && isset($_POST['inputLanguage']) && isset($_POST['inputAvatar']) && isset($_POST['inputVideo']) && isset($_POST['inputQuote']) && isset($_POST['inputQuoteAuthor'])) {
+                $this->insert();
+            } else {
+                echo "All fields are required.";
+            }
         }
-
         require 'view/homepage.php';
     }
 
@@ -33,6 +34,23 @@ class controller {
 
         $preparation = $connection->prepare("INSERT INTO student (first_name, last_name, username, linkedin, github, email, preferred_language, avatar, video, quote, quote_author) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         $preparation->bindParam(':first_name', $firstName, PDO::PARAM_STR, 50);
-        $preparation->bindParam()
+        $preparation->bindParam(':last_name', $lastName, PDO::PARAM_STR, 50);
+        $preparation->bindParam(':username', $userName, PDO::PARAM_STR, 50);
+        $preparation->bindParam(':linkedin', $linkedIn, PDO::PARAM_STR, 255);
+        $preparation->bindParam(':github', $github, PDO::PARAM_STR, 255);
+        $preparation->bindParam(':email', $email, PDO::PARAM_STR, 50);
+        $preparation->bindParam(':preferred_language', $language, PDO::PARAM_STR, 2);
+        $preparation->bindParam(':avatar', $avatar, PDO::PARAM_STR,255);
+        $preparation->bindParam(':video', $video, PDO::PARAM_STR, 255);
+        $preparation->bindParam(':quote', $quote, PDO::PARAM_STR, 255);
+        $preparation->bindParam(':quote_author', $quoteAuthor, PDO::PARAM_STR, 50);
+        try {
+            $preparation->execute();
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+            echo $e->getCode();
+        }
+
     }
 }
--- view/homepage.php
+++ view/homepage.php
@@ -12,38 +12,38 @@
         <!-- First, last and user name -->
         <div class="form-group">
             <label for="inputFirstName">First name</label>
-            <input type="text" id="inputFirstName" class="form-control">
+            <input type="text" id="inputFirstName" name ="inputFirstName" class="form-control" required>
             <label for="inputLastName">Last name</label>
-            <input type="text" id="inputLastName" class="form-control">
+            <input type="text" id="inputLastName" name="inputLastName" class="form-control" required>
             <label for="inputUserName">User name</label>
-            <input type="text" id="inputUserName" class="form-control">
+            <input type="text" id="inputUserName" name="inputUserName" class="form-control" required>
         </div>
         <!-- Linkedin, Github, Email and language -->
         <div class="form-group">
             <label for="inputLinkedin">Linkedin</label>
-            <input type="text" id="inputLinkedin" class="form-control">
+            <input type="text" id="inputLinkedin" name="inputLinkedin" class="form-control" required>
             <label for="inputGithub">Github</label>
-            <input type="text" id="inputGithub" class="form-control">
+            <input type="text" id="inputGithub" name="inputGithub" class="form-control" required>
             <label for="inputEmail">Email</label>
-            <input type="email" id="inputEmail" class="form-control">
+            <input type="email" id="inputEmail" name="inputEmail" class="form-control" required>
             <label for="inputLanguage">Preferred language</label>
-            <select class="form-control" id="inputLanguage">
-                <option>EN</option>
-                <option>NL</option>
-                <option>DE</option>
-                <option>FR</option>
+            <select class="form-control" id="inputLanguage" name="inputLanguage">
+                <option value="en">EN</option>
+                <option value="nl">NL</option>
+                <option value="de">DE</option>
+                <option value="fr">FR</option>
             </select>
         </div>
         <!-- Avatar, video and quote -->
         <div class="form-group">
             <label for="inputAvatar">Avatar</label>
-            <input type="text" id="inputAvatar" class="form-control">
+            <input type="text" id="inputAvatar" name="inputAvatar" class="form-control" required>
             <label for="inputVideo">Video</label>
-            <input type="text" id="inputVideo" class="form-control">
+            <input type="text" id="inputVideo" name="inputVideo" class="form-control" required>
             <label for="inputQuote">Quote</label>
-            <input type="text" id="inputQuote" class="form-control">
+            <input type="text" id="inputQuote" name="inputQuote" class="form-control" required>
             <label for="inputQuoteAuthor">Quote author</label>
-            <input type="text" id="inputQuoteAuthor" class="form-control">
+            <input type="text" id="inputQuoteAuthor" name="inputQuoteAuthor" class="form-control" required>
         </div>
         <button type="submit" class="btn btn-primary">GO</button>
     </form>
