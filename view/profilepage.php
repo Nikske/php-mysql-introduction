431d2c83e2a220859d3fa8e9f6285d3fe33fb4aa
Nikske <wolfvdd@gmail.com>
2020-02-26 11:49:00 +0100

    profile page updates

--- view/profilepage.php
+++ view/profilepage.php
@@ -10,20 +10,31 @@
     <div class="container">
         <section class="row">
             <!-- Avatar and language -->
-            <article class="col">
+            <article class="col bg-info m-3 p-2 rounded">
+                <img src="<?php echo $user['avatar'] ?>">
             </article>
             <!-- User, First and Last name -->
-            <article class="col">
-                <?php echo $user['first_name'] ?>
+            <article class="col bg-info m-3 p-2 rounded">
+                <strong><p><?php echo $user['username']?></p></strong>
+                <p><?php echo $user['first_name'] ?> <?php echo $user['last_name'] ?></p>
+                <p><?php echo $user['created_at'] ?></p>
             </article>
         </section>
 
         <section class="row">
             <!-- Video and quote -->
-            <article class="col">
+            <article class="col bg-info m-3 p-2 rounded">
+                <a target="_blank" href="<?php echo $user['video'] ?>"><?php $user['video'] ?></a>
+                <blockquote class="blockquote">
+                    &quot;<?php echo $user['quote'] ?>&quot;
+                    <footer class="blockquote-footer"><?php echo $user['quote_author'] ?></footer>
+                </blockquote>
             </article>
             <!-- Links and email -->
-            <article class="col">
+            <article class="col bg-info m-3 p-2 rounded">
+                <a target="_blank" href="<?php echo $user['linkedin']?>"><p><?php echo $user['linkedin'] ?></p></a>
+                <a target="_blank" href="<?php echo $user['github']?>"><p><?php echo $user['github'] ?></p></a>
+                <p><?php echo $user['email'] ?></p>
             </article>
         </section>
     </div>
